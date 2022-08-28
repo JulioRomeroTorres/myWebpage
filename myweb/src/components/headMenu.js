f5432ed68270ace777bc3d08f3741acc76b1e580
JulioRomeroTorres <jcromerot@uni.pe>
2022-08-27 23:36:01 -0500

    test a little change

--- myweb/src/components/headMenu.js
+++ myweb/src/components/headMenu.js
@@ -14,6 +14,7 @@ export function Headmenu(props){
                 Contact me
             </button>
             <h1>Hi, Mi name is Julio Cesar Romero Torres </h1>
+            <h3> It is my first time as FullStack Developer</h3>
             <img src = {require(`../images/others/personalPhoto.png`)} alt = 'My personal photo' />
         </div>
     );
